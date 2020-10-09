e24e6ecf30dc0c95e86ebf4a3ecfdb7d2a4320e0
zzzzzzzsu <js282kk@korea.ac.kr>
2020-10-09 12:52:45 +0000

    Add 1010-1017 problems

---
+++ src/201009_1010-1017.py
@@ -0,0 +1,39 @@
+# 1010
+num = input()
+num = int(num)
+
+print(num)
+
+# 1011
+mon = input()
+mon = str(mon)
+
+print(mon)
+
+# 1012
+num = input()
+num = float(num)
+
+print('%0.6f' %num)
+
+# 1013
+num1, num2 = input().split()
+print(num1 + ' ' + num2)
+
+
+# 1014
+mon1, mon2 = input().split()
+print(mon2 + ' ' +mon1)
+
+# 1015
+num = input()
+num = float(num)
+
+num1 = round(num,3)
+print('%0.2f' %num1)
+
+# 1017
+num = input()
+num = int(num)
+
+print('%d %d %d' %(num, num, num))
