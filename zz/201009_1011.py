c8ceaf6081ae156008e97d949d8cb7f661ad834b
repl.it user <replituser@example.com>
2020-10-09 18:29:50 +0000

    Solve 1010 - 1017

---
+++ zz/201009_1011.py
@@ -0,0 +1,7 @@
+def main():
+  character = input()
+  print(character)
+
+
+if __name__ ==  '__main__':
+	main()
---
+++ zz/201009_1015.py
@@ -0,0 +1,7 @@
+def main():
+  float_value = float(input())
+  print("%.2f" % round(float_value, 2))
+
+
+if __name__ ==  '__main__':
+	main()
