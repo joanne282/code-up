f2ceb517d98672f1e3bdd5566ba7231239212018
zzzzzzzsu <js282kk@korea.ac.kr>
2020-10-16 06:18:35 +0000

    Add 1047-58 problems

---
+++ src/201016_1053-58.py
@@ -0,0 +1,58 @@
+# 1053
+num = input()
+num = int(num)
+
+if num == 0:
+  print(1)
+else:
+  print(0)
+
+# 1054
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 == 1 and num2 ==1:
+  print(1)
+else:
+  print(0)
+
+# 1055
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 == 1 or num2 == 1:
+  print(1)
+else:
+  print(0)
+
+# 1056
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 != num2:
+  print(1)
+else:
+  print(0)
+
+# 1057
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 == num2:
+  print(1)
+else:
+  print(0)
+
+# 1058
+num1, num2 = input().split()
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 == 0 and num2 == 0:
+  print(1)
+else:
+  print(0)
